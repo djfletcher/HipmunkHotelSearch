c50c92e68a7cae8218c2b3d1fbbd8a04ef9c6499
Daniel <daniel.j.fletcher2@gmail.com>
2017-06-13 13:11:53 -0700

    Factor out Aggregator class into separate file

--- hotel_search/merged_api.rb
+++ hotel_search/merged_api.rb
@@ -1,44 +1,10 @@
 require 'sinatra'
 require 'rest-client'
 require 'json'
+require_relative 'aggregator'
 
 set :port, 8000
 
 get '/hotels/search' do
   Aggregator.aggregate
-end
-
-
-class Aggregator
-  
-  APIS = [
-    'expedia',
-    'orbitz',
-    'priceline',
-    'travelocity',
-    'hilton'
-  ]
-
-  def self.aggregate
-    all = []
-
-    APIS.each do |api|
-      all += Aggregator.get(api)
-    end
-
-    results = { 'results' => Aggregator.sort(all) }
-    JSON.generate(results)
-  end
-
-  def self.get(api)
-    raw_response = RestClient.get "http://localhost:9000/scrapers/#{api}"
-    JSON.parse(raw_response)['results']
-  end
-
-  def self.sort(results)
-    results.sort do |res1, res2|
-      res2['ecstasy'] <=> res1['ecstasy']
-    end
-  end
-
 end
